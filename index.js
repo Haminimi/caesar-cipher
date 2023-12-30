4c32fcf4eda7eeb522d77c8e705f0533d27aaa77
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-12-30 21:14:52 +0100

    Add the reverseString function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export function capitalize(sentence) {
 	return sentence.charAt(0).toUpperCase() + sentence.slice(1);
 }
+
+export function reverseString(string) {
+	return string.split('').reverse().join('');
+}
