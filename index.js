c77fc987c23dc6a2159502c02d146eb5a95788d6
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-12-30 21:16:41 +0100

    Create the calculator object

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ export function capitalize(sentence) {
 export function reverseString(string) {
 	return string.split('').reverse().join('');
 }
+
+export const calculator = {
+	add: (a, b) => {
+		return a + b;
+	},
+
+	subtract: (a, b) => {
+		return a - b;
+	},
+
+	multiply: (a, b) => {
+		return a * b;
+	},
+
+	divide: (a, b) => {
+		return a / b;
+	},
+};
